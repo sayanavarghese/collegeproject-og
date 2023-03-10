74509d5a5676161ec86499a7fa541357a7db29f6
sayana varghese <sayanavarghese2002@gmail.com>
2023-03-10 10:00:58 +0530

    adding changes

--- jobfinder/page_app/models.py
+++ jobfinder/page_app/models.py
@@ -2,20 +2,19 @@ from django.db import models
 
 # Create your models here.
 class job(models.Model):
-    title=models.varchar(225)
-    position=models.varchar(225)
-    category=models.varchar(225)
-    location=models.varchar(225)
-    description=models.varchar(225)
-    userld=models.int()
-    salary=models.varchar(225)
+    title=models.TextField(max_length=225)
+    position=models.TextField(max_length=225)
+    category=models.TextField(max_length=225)
+    location=models.TextField(max_length=225)
+    description=models.TextField(max_length=225)
+    userld=models.IntegerField()
+    salary=models.TextField(max_length=225)
 class resume(models.Model):
-    title=models.varchar(225)
-    name=models.varchar(225)
-    email=models.varchar(225)
-    address=models.varchar(225)
-    phone no=models.varchar(225)
-    experiance=models.varchar(225)
+    title=models.TextField(max_length=225)
+    name=models.TextField(max_length=225)
+    email=models.EmailField(max_length=225)
+    address=models.TextField(max_length=225)
+    experiance=models.TextField(max_length=225)
     
 
 
