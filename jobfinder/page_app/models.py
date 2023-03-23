7e6ea428d4a28a38b6e778126f2bdee7ea98096c
sayana varghese <sayanavarghese2002@gmail.com>
2023-03-23 17:25:50 +0530

    Adding changes

--- jobfinder/page_app/models.py
+++ jobfinder/page_app/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
+Create your models here.
 class Job(models.Model):
     title=models.TextField(max_length=225)
     position=models.TextField(max_length=225)
