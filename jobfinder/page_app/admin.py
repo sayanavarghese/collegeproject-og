985e177fc296fbf7f0ed94fd345e8e0af43cfbd9
sayana varghese <sayanavarghese2002@gmail.com>
2023-03-23 17:05:12 +0530

    Adding changes

--- jobfinder/page_app/admin.py
+++ jobfinder/page_app/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from .models import Profile
+from .models import Profile,Job,Resume,Application
+
 
 class ProfileAdmin(admin.ModelAdmin):
     list_filter =("id","user_type")
     list_display =("id","user_type")
 
 admin.site.register(Profile,ProfileAdmin)
+admin.site.register(Job)
+admin.site.register(Resume)
+admin.site.register(Application)
--- jobfinder/page_app/models.py
+++ jobfinder/page_app/models.py
@@ -2,20 +2,30 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-# class job(models.Model):
-#     title=models.TextField(max_length=225)
-#     position=models.TextField(max_length=225)
-#     category=models.TextField(max_length=225)
-#     location=models.TextField(max_length=225)
-#     description=models.TextField(max_length=225)
-#     userld=models.IntegerField()
-#     salary=models.TextField(max_length=225)
-# class resume(models.Model):
-#     title=models.TextField(max_length=225)
-#     name=models.TextField(max_length=225)
-#     email=models.EmailField(max_length=225)
-#     address=models.TextField(max_length=225)
-#     experiance=models.TextField(max_length=225)
+class Job(models.Model):
+    title=models.TextField(max_length=225)
+    position=models.TextField(max_length=225)
+    category=models.TextField(max_length=225)
+    location=models.TextField(max_length=225)
+    description=models.TextField(max_length=225)
+    userld=models.IntegerField()
+    salary=models.TextField(max_length=225)
+    Create_at=models.DateTimesField(auto_now_add=True)
+    
+class Resume(models.Model):
+    title=models.TextField(max_length=225)
+    name=models.TextField(max_length=225)
+    email=models.EmailField(max_length=225)
+    address=models.TextField(max_length=225)
+    experiance=models.TextField(max_length=225)
+    qualification=models.TextField(max_length=225)
+    phone_no=models.IntegerField(max_length=12)
+    about_me=models.TextField(max_length=225)
+class Application(models.Model):
+        applied_by=models.Forignkey(User,on_delete=models.CASCADE)
+        job=models.models.Forignkey(User,on_delete=models.CASCADE)
+        Create_at=models.DateTimesField(auto_now_add=True)
+
 class Profile(models.Model):
         CHOICES = (
         ("ADMIN", "ADMIN"),
